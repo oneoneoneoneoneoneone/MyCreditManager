f4a571ac2ea51f45d3eeabd964c821f97c430540
oneoneoneoneoneoneone <0ne1_1@naver.com>
2023-05-10 18:33:06 +0900

    [Refactor] print할 원시값 -> enum description 추가

--- MyCreditManager/main.swift
+++ MyCreditManager/main.swift
@@ -19,85 +19,104 @@ class Main{
     func startMyCreditManager(){
         while true{
             do{
-                print("원하는 기능을 입력해주세요")
-                print("1: 학생추가, 2. 학생삭제, 3: 성적추가(변경), 4: 성적삭제, 5: 평점보기, X: 종료")
+                printTaskHelp()
                 let inputData = readLine()!
                 
-                try commandOperate(inputData)
-                
-            }catch InputError.wrongStartValue{
-                print("뭔가 입력이 잘못되었습니다. 1~5 사이의 숫자 혹은 X를 입력해주세요.")
-            }
-            catch InputError.wrongValue{
-                print("입력이 잘못되었습니다. 다시 확인해주세요.")
-            }
-            catch InputError.duplicationStudent(student: let student){
-                print("\(student) 학생은 이미 존재하는 학생입니다. 추가하지 않습니다.")
-            }
-            catch InputError.nonexistentStudent(student: let student){
-                print("\(student) 학생을 찾지 못했습니다.")
-            }
-            catch InputError.Exit{
-                print("프로그램을 종료합니다...")
-                return
-            }
-            catch{
-                print("알 수 없는 에러로 프로그램을 종료합니다...")
-                return
+                if try orderTask(inputData) == false {
+                    return
+                }
+            }catch{
+                print(error)
             }
         }
     }
     
-    private func commandOperate(_ command: String) throws {
-        if let task = Task(rawValue: command){
-            switch task{
-            case .studentAdd:
-                print("추가할 학생의 이름을 입력해주세요")
-                let inputData = readLine()!
-                
-                try studentGradeManager?.addStudent(inputData)
-                
-            case .studentRemove:
-                print("삭제할 학생의 이름을 입력해주세요")
-                let inputData = readLine()!
-                
-                try studentGradeManager?.removeStudent(inputData)
-                
-            case .gradeAdd:
-                print("성적을 추가할 학생의 이름, 과목 이름, 성적(A+, A, F 등)을 띄어쓰기로 구분하여 차례로 작성해주세요.\n입력예) Micky Swift A+\n만약에 학생의 성적 중 해당 과목이 존재하면 기존 점수가 갱신됩니다.")
-                let inputData = readLine()!
-                
-                try studentGradeManager?.addGrade(inputData)
-                
-            case .gradeRemove:
-                print("성적을 삭제할 학생의 이름, 과목 이름을 띄어쓰기로 구분하여 차례로 작성해주세요.\n입력예) Mickey Swift")
-                let inputData = readLine()!
-                
-                try studentGradeManager?.removeGrade(inputData)
-                
-            case .getAverage:
-                print("평점을 알고싶은 학생의 이름을 입력해주세요")
-                let inputData = readLine()!
-                
-                try studentGradeManager?.getAverage(inputData)
-                
-            case .exit:
-                throw InputError.Exit
-            }
-        }
-        else{
+    private func orderTask(_ inputCommand: String) throws -> Bool{
+        guard let task = Task(rawValue: inputCommand) else {
             throw InputError.wrongStartValue
         }
+        print(task.description)
+        if task == .exit{
+            return false
+        }
+        
+        let inputData = readLine()
+        
+        switch task{
+        case .studentAdd:
+            try studentGradeManager?.addStudent(inputData)
+        case .studentRemove:
+            try studentGradeManager?.removeStudent(inputData)
+        case .gradeAdd:
+            try studentGradeManager?.addGrade(inputData)
+        case .gradeRemove:
+            try studentGradeManager?.removeGrade(inputData)
+        case .getAverage:
+            try studentGradeManager?.getAverage(inputData)
+        case .exit:
+            return false
+        }
+        return true
+    }
+    
+    func printTaskHelp(){
+        print("원하는 기능을 입력해주세요")
+        print(Task.allCases.map{"\($0.rawValue): \($0.title)"}.joined(separator: ", "))
     }
 }
 
+
 //MARK: Enum Task
 
-enum Task: String{
+enum Task: String, CaseIterable{
     case studentAdd = "1"
     case studentRemove = "2"
     case gradeAdd = "3"
     case gradeRemove = "4"
     case getAverage = "5"
     case exit = "X"
+    
+    var title: String{
+        switch self{
+        case .studentAdd:
+            return "학생추가"
+        case .studentRemove:
+            return "학생삭제"
+        case .gradeAdd:
+            return "성적추가(변경)"
+        case .gradeRemove:
+            return "성적삭제"
+        case .getAverage:
+            return "평점보기"
+        case .exit:
+            return "종료"
+        }
+    }
+    
+    var description: String {
+        switch self{
+        case .studentAdd:
+            return "추가할 학생의 이름을 입력해주세요"
+        case .studentRemove:
+            return "삭제할 학생의 이름을 입력해주세요"
+        case .gradeAdd:
+            return "성적을 추가할 학생의 이름, 과목 이름, 성적(A+, A, F 등)을 띄어쓰기로 구분하여 차례로 작성해주세요.\n입력예) Micky Swift A+\n만약에 학생의 성적 중 해당 과목이 존재하면 기존 점수가 갱신됩니다."
+        case .gradeRemove:
+            return "성적을 삭제할 학생의 이름, 과목 이름을 띄어쓰기로 구분하여 차례로 작성해주세요.\n입력예) Mickey Swift"
+        case .getAverage:
+            return "평점을 알고싶은 학생의 이름을 입력해주세요"
+        case .exit:
+            return "프로그램을 종료합니다..."
+        }
+    }
+    
+//    var result: String{
+//        switch self{
+//
+//        case .studentAdd(let a: String):
+//            return "aa"
+//        default: break
+//
+//        }
+//    }
 }
