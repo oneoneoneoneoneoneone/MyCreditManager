8284ea20542c06ece6ed56bab2f79587a1f9d395
oneoneoneoneoneoneone <0ne1_1@naver.com>
2023-05-10 18:31:28 +0900

    [Refactor] throw 분기 처리 if 문 -> guard 문 / 평점 합 reduce

--- MyCreditManager/StudentGradeManager.swift
+++ MyCreditManager/StudentGradeManager.swift
@@ -8,54 +8,57 @@
 import Foundation
 
 protocol StudentGradeManagerProtocol{
-    func addStudent(_ studentName: String) throws
-    func removeStudent(_ studentName: String) throws
-    func addGrade(_ studentGrade: String) throws
-    func removeGrade(_ studentGrade: String) throws
-    func getAverage(_ studentName: String) throws
+    func addStudent(_ inputName: String?) throws
+    func removeStudent(_ inputName: String?) throws
+    func addGrade(_ inputGrade: String?) throws
+    func removeGrade(_ inputGrade: String?) throws
+    func getAverage(_ inputName: String?) throws
 }
 
 class StudentGradeManager: StudentGradeManagerProtocol{
     private var students: [Student] = []
     private var studentGrades: [StudentGrade] = []
     
-    func addStudent(_ studentName: String) throws{
-        if studentName == "" || studentName.contains(" ") {
+    func addStudent(_ inputName: String?) throws{
+        guard let studentName = inputName,
+              studentName != "", studentName.contains(" ") == false else {
             throw InputError.wrongValue
         }
+        
         //학생 찾기
         if students.first(where: {$0.name == studentName}) != nil {
-            throw InputError.duplicationStudent(student: studentName)
+            throw InputError.duplicatedStudent(name: studentName)
         }
         
         students.append(Student(name: studentName))
-        
         print("\(studentName) 학생을 추가했습니다.")
     }
     
-    func removeStudent(_ studentName: String) throws{
-        if studentName == "" || studentName.contains(" ") {
+    func removeStudent(_ inputName: String?) throws{
+        guard let studentName = inputName,
+              studentName != "", studentName.contains(" ") == false else {
             throw InputError.wrongValue
         }
+        
         //학생 찾기
         if students.first(where: {$0.name == studentName}) == nil {
-            throw InputError.nonexistentStudent(student: studentName)
+            throw InputError.nonexistentStudent(name: studentName)
         }
         
         students.removeAll(where: {$0.name == studentName})
-        
         print("\(studentName) 학생을 삭제했습니다.")
     }
 
-    func addGrade(_ studentGrade: String) throws{
-        if studentGrade == "" {
+    func addGrade(_ inputGrade: String?) throws{
+        guard let inputGrade = inputGrade,
+              inputGrade != "" else {
             throw InputError.wrongValue
         }
-        //쪼개기
-        let studentGrade = studentGrade.components(separatedBy: " ")
-        if studentGrade.count != 3{
+        guard inputGrade.components(separatedBy: " ").count == 3 else{
             throw InputError.wrongValue
         }
+        let studentGrade = inputGrade.components(separatedBy: " ")
+        
         //성적값 검증. Regex literal
         let regex = /^[A-D][+]$|^[A-F]$/
         guard let grade = try regex.firstMatch(in: studentGrade[2])?.output else {
@@ -63,53 +66,53 @@ class StudentGradeManager: StudentGradeManagerProtocol{
         }
         //학생 찾기
         guard let student = students.first(where: {$0.name == studentGrade[0]}) else {
-            throw InputError.nonexistentStudent(student: studentGrade[0])
+            throw InputError.nonexistentStudent(name: studentGrade[0])
         }
         
         if studentGrades.first(where: {$0.student.name == student.name && $0.subject == studentGrade[1]}) != nil{
             studentGrades.removeAll(where: {$0.student.name == student.name && $0.subject == studentGrade[1]})
         }
-        studentGrades.append(StudentGrade(student: student, subject: studentGrade[1], grade: String(grade)))
         
+        studentGrades.append(StudentGrade(student: student, subject: studentGrade[1], grade: String(grade)))
         print("\(studentGrade[0]) 학생의 \(studentGrade[1]) 과목이 \(grade)로 추가(변경)되었습니다.")
     }
 
-    func removeGrade(_ studentGrade: String) throws{
-        if studentGrade == "" {
+    func removeGrade(_ inputGrade: String?) throws{
+        guard let inputGrade = inputGrade,
+              inputGrade != "" else {
             throw InputError.wrongValue
         }
-        //쪼개기
-        let studentGrade = studentGrade.components(separatedBy: " ")
-        if studentGrade.count != 2{
+        guard inputGrade.components(separatedBy: " ").count == 2 else{
             throw InputError.wrongValue
         }
+        let studentGrade = inputGrade.components(separatedBy: " ")
+
         //학생 찾기
         guard let student = students.first(where: {$0.name == studentGrade[0]}) else {
-            throw InputError.nonexistentStudent(student: studentGrade[0])
+            throw InputError.nonexistentStudent(name: studentGrade[0])
         }
         
         studentGrades.removeAll(where: {$0.student.name == student.name && $0.subject == studentGrade[1]})
-        
         print("\(studentGrade[0]) 학생의 \(studentGrade[1]) 과목의 성적이 삭제되었습니다.")
     }
 
-    func getAverage(_ studentName: String) throws{
-        if studentName == "" || studentName.contains(" ") {
+    func getAverage(_ inputName: String?) throws{
+        guard let studentName = inputName,
+              studentName != "", studentName.contains(" ") == false else {
             throw InputError.wrongValue
         }
+        
         //학생 찾기
         guard let student = students.first(where: {$0.name == studentName}) else {
-            throw InputError.nonexistentStudent(student: studentName)
+            throw InputError.nonexistentStudent(name: studentName)
         }
         //성적 찾기
         let studentGrades = studentGrades.filter({$0.student.name == student.name})
         
-        var totalScore = 0.0
-        studentGrades.forEach{sg in
-            totalScore += getNuberScore(sg.grade)
-            
-            print("\(sg.subject) : \(sg.grade)")
-        }
+        let totalScore = studentGrades.map{getNuberScore($0.grade)}
+            .reduce(0){$0 + $1}
+        
+        studentGrades.forEach{print("\($0.subject) : \($0.grade)")}
         print(String(format: "평점 : %.2f", totalScore/Double(studentGrades.count)))
     }
     
